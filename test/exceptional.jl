5d0420d0f99a5ae7e31e321902b7f4a37e8dc1bb
Mus M <mus-m@outlook.com>
2016-10-12 03:46:00 -0400

    Try to fix travis for NaN case

--- test/exceptional.jl
+++ test/exceptional.jl
@@ -1,39 +1,34 @@
-const Test = Amal
-const Ref = Base
-
 @testset "denormal/nonnumber $T" for T in (Float64, Float32,)
 
-    @test Test.exp(T(-Inf)) === T(0)
-    @test Test.exp(T(Inf)) === T(Inf)
-    @test Test.exp(T(NaN)) === T(NaN)
-    @test Test.exp(T(0)) === T(1) # exact
+    @test isnan(Amal.exp(T(NaN)))
+    @test Amal.exp(T(-Inf)) === T(0)
+    @test Amal.exp(T(Inf)) === T(Inf)
+    @test Amal.exp(T(0)) === T(1) # exact
 
     for x in T[10000, -10000]
-        @test cmpdenorm(T, Test.exp(x), Ref.exp(BigFloat(x)))
+        @test cmpdenorm(T, Amal.exp(x), Base.exp(BigFloat(x)))
     end
 
-
-    @test Test.exp2(T(-Inf)) === T(0)
-    @test Test.exp2(T(Inf)) === T(Inf)
-    @test Test.exp2(T(NaN)) === T(NaN)
-    @test Test.exp2(T(0)) === T(1) # exact
-    @test Test.exp2(T(2)) === T(4)
-    @test Test.exp2(T(12)) === T(4096)
+    @test isnan(Amal.exp2(T(NaN)))
+    @test Amal.exp2(T(-Inf)) === T(0)
+    @test Amal.exp2(T(Inf)) === T(Inf)
+    @test Amal.exp2(T(0)) === T(1) # exact
+    @test Amal.exp2(T(2)) === T(4)
+    @test Amal.exp2(T(12)) === T(4096)
 
     for x in T[10000, -10000]
-        @test cmpdenorm(T, Test.exp2(x), Ref.exp2(BigFloat(x)))
+        @test cmpdenorm(T, Amal.exp2(x), Base.exp2(BigFloat(x)))
     end
 
-
-    @test Test.exp10(T(-Inf)) === T(0)
-    @test Test.exp10(T(Inf)) === T(Inf)
-    @test Test.exp10(T(NaN)) === T(NaN)
-    @test Test.exp10(T(0)) === T(1) # exact
-    @test Test.exp10(T(1)) === T(10)
-    @test Test.exp10(T(3)) === T(1000)
+    @test isnan(Amal.exp10(T(NaN)))
+    @test Amal.exp10(T(-Inf)) === T(0)
+    @test Amal.exp10(T(Inf)) === T(Inf)
+    @test Amal.exp10(T(0)) === T(1) # exact
+    @test Amal.exp10(T(1)) === T(10)
+    @test Amal.exp10(T(3)) === T(1000)
 
     for x in T[10000, -10000]
-        @test cmpdenorm(T, Test.exp10(x), Ref.exp10(BigFloat(x)))
+        @test cmpdenorm(T, Amal.exp10(x), Base.exp10(BigFloat(x)))
     end
 
 end
